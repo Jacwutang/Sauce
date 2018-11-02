ad52ab36790b42dfa8c5e19cae183b47390928cb
Jack Wu <jackwu350@yahoo.com>
2018-11-02 15:44:15 -0700

    use heroku env port

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ app.get("/", (request, response) => {
 
 app.use(express.static(path.join(__dirname, "/frontend/public")));
 
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port);

2e9376aad99a90a765705df70c452b3cfa10ead1
Jack Wu <jackwu350@yahoo.com>
2018-11-02 15:36:51 -0700

    modify path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (request, response) => {
   response.send("Hello from Express!");
 });
 
-app.use(express.static(path.join(__dirname, "frontend/public")));
+app.use(express.static(path.join(__dirname, "/frontend/public")));
 
 const port = 5000 || process.env.PORT;
 app.listen(port);

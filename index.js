f6205ca8a4a0735463bb67506e7deaaaf05825b5
Jack Wu <jackwu350@yahoo.com>
2018-11-03 21:19:56 -0700

    favicon move to public folder

--- index.js
+++ index.js
@@ -27,8 +27,6 @@ app.get("/api/health-check/", (request, response) => {
   response.sendStatus(200);
 });
 
-app.get("/favicon.ico", (req, res) => res.status(204));
-
 const pollingHelper = () => {
   // Interval keep going
   // intervalId =
